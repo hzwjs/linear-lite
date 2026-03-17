f9161f1f87d78104aca3bbbdd7bc191130d20b7c
huangzhiwen <hzw_js@126.com>
2026-03-17 11:26:20 +0800

    feat: add locale store and persistence

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import "./style.css";
 import App from "./App.vue";
 import router, { setupRouterGuards } from "./router";
 import { i18n } from "./i18n";
+import { useLocaleStore } from "./store/localeStore";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
+useLocaleStore();
 app.use(i18n);
 app.use(router);
 setupRouterGuards(pinia);
